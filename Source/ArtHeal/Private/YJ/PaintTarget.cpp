b0a6d2ed4d1807f2ee4bdae6fcd18c97ef1f4ecc
silverlnng <112385982+silverlnng@users.noreply.github.com>
2024-08-20 18:30:18 +0900

    Paint 기본완료

---
+++ Source/ArtHeal/Private/YJ/PaintTarget.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "YJ/PaintTarget.h"
+#include "ImageUtils.h"
+#include "Engine/Canvas.h"
+#include "Kismet/KismetRenderingLibrary.h"
+
+// Sets default values
+APaintTarget::APaintTarget()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	sceneComponent =CreateDefaultSubobject<USceneComponent>(TEXT("USceneComponent"));
+	SetRootComponent(sceneComponent);
+	
+	
+	staticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	staticMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void APaintTarget::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//만들고 CLEAR 해서 넣어줌 .
+	
+	CRT_PaintMask = UKismetRenderingLibrary::CreateRenderTarget2D(GetWorld(),1024,1024,RTF_RGBA32f);
+	
+	UKismetRenderingLibrary::ClearRenderTarget2D(GetWorld(),CRT_PaintMask,FLinearColor(1,1,1,1));
+
+	
+	OriginMat =staticMesh->GetMaterial(0);
+	
+	TempMat =UMaterialInstanceDynamic::Create(OriginMat,this);
+
+	staticMesh->SetMaterial(0,TempMat);
+	
+	TempMat->SetTextureParameterValue(FName(TEXT("PaintTexture")),CRT_PaintMask);
+	
+}
+
+// Called every frame
+void APaintTarget::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APaintTarget::Painted(int num,FVector2D DrawLocation,float brushSize)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("%s(%d) : press Fire"), *FString(__FUNCTION__), __LINE__);
+	UMaterialInterface* brushTexture;
+	FVector2D brushLocation;
+	
+	brushTexture = BrushMatArray[num];
+	//UKismetRenderingLibrary::DrawMaterialToRenderTarget(GetWorld(),CRT_DamageMask,MI_PaintTexture);
+	
+	UCanvas* Canvas;
+	FVector2D size;
+	FDrawToRenderTargetContext context;
+	
+	UKismetRenderingLibrary::BeginDrawCanvasToRenderTarget(GetWorld(),CRT_PaintMask,Canvas,size,context);
+	
+	brushLocation.X = (size.X*DrawLocation.X)-brushSize*0.5f;
+	brushLocation.Y = (size.Y*DrawLocation.Y)-brushSize*0.5f;
+	Canvas->K2_DrawMaterial(brushTexture,brushLocation,FVector2D(brushSize),FVector2D(0,0),FVector2D(1,1));
+
+	UKismetRenderingLibrary::EndDrawCanvasToRenderTarget(GetWorld(),context);
+
+}
+
+void APaintTarget::SaveTexture(UTextureRenderTarget2D* TexRT)
+{
+	// 호출히면 저장하는 함수
+	//FImageUtils::ExportRenderTarget2DAsPNG(TexRT, FArchive& Ar);
+}
+
---
+++ Source/ArtHeal/Public/YJ/PaintTarget.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PaintTarget.generated.h"
+
+UCLASS()
+class ARTHEAL_API APaintTarget : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APaintTarget();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	class USceneComponent* sceneComponent;
+	
+	UPROPERTY(EditAnywhere, Category = "Paint")
+	class UStaticMeshComponent* staticMesh;
+	
+	UPROPERTY(EditAnywhere, Category = "Paint")
+	UTextureRenderTarget2D* CRT_PaintMask;
+
+	UPROPERTY(EditAnywhere, Category = "Paint")
+	UMaterialInterface* OriginMat;
+
+	UPROPERTY(EditAnywhere, Category = "Paint")
+	UMaterialInstanceDynamic* TempMat;
+
+	UPROPERTY(EditAnywhere, Category = "Paint")
+	TArray<UMaterialInterface*> BrushMatArray;
+	
+	void Painted(int num,FVector2D DrawLocation,float brushSize);
+
+	void SaveTexture(UTextureRenderTarget2D* TexRT);
+};
